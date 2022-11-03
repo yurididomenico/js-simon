dce4654c5dfb693b8de6cd26b0049dd3fc68c8e8
yurididomenico <yurididomenico93@gmail.com>
2022-11-03 15:49:31 +0100

    JS: Funzione richiesta numeri

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,45 +10,78 @@ console.log("YDD");
 -Il software dice quanti e quali dei numeri sono stati individuati.
 */
 
-//Variabili 5 numeri - random
+//Variabili 5 numeri - random - numeri inseriti dall'utente
 let numeri = document.getElementById('stampaNumeri');
 let random = [];
+let utenteNumeri = [];
 
-//Ciclo genera 5 numeri random - stampa
-for(let i=0; i<5; i++)
+function simonGame()
 {
-    random[i] = Math.round(Math.random()*5)+1;
-    numeri.innerHTML += `<span class="border border-2 border-dark p-3">${random[i]}</span>`;
+    //Ciclo genera 5 numeri random - stampa
+    for(let i=0; i<5; i++)
+    {
+        random[i] = Math.round(Math.random()*5)+1;
+        numeri.innerHTML += `<span class="border border-2 border-dark p-3">${random[i]}</span>`;
+    }
+
+    //Dopo 10 secondi scompaiono i numeri
+    setTimeout(tempo10, 3000);
+
+    //Dopo 11 secondi appare la richiesta numeri
+    setTimeout(richiestaNumeri, 4000);
+
+    
+    
+
+    
+    
+
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Timer 10 secondi
-setTimeout(tempo10, 10000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function tempo10()
 {
     numeri.innerHTML = "";
+}
+
+//Richiesta 5 numeri
+function richiestaNumeri()
+{
+    for(let i=0; i<5; i++)
+    {
+        utenteNumeri[i] = parseInt(prompt(`Numero ${i+1}: `));
+        
+    }
+    
+    for(let i=0; i<5; i++)
+    {
+        document.getElementById('test').innerHTML += `<span class="border border-2 border-dark p-3">${utenteNumeri[i]}</span>`;
+    }
+    return utenteNumeri;
+}
+
+
+function refresh()
+{
+    location.reload();
 }
 // ----- FINE :) -----
