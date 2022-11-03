ccf654f7a9a35fdb7da2ed2d842761595e6f45ea
yurididomenico <yurididomenico93@gmail.com>
2022-11-03 15:01:22 +0100

    JS: Funzione timer 10 secondi

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,7 +4,7 @@ console.log("YDD");
 
 /*
 -Visualizzare in pagina 5 numeri casuali.
--Da lì parte un timer di 30 secondi.
+-Da lì parte un timer di 10 secondi.
 -Dopo 10 secondi i numeri scompaiono
 -L'utente deve inserire i numeri che ha visto precedentemente tramite il prompt().
 -Il software dice quanti e quali dei numeri sono stati individuati.
@@ -14,15 +14,15 @@ console.log("YDD");
 let numeri = document.getElementById('stampaNumeri');
 let random = [];
 
-numeri.innerHTML = "Test";
-
 //Ciclo genera 5 numeri random - stampa
 for(let i=0; i<5; i++)
 {
     random[i] = Math.round(Math.random()*5)+1;
+    numeri.innerHTML += `<span class="border border-2 border-dark p-3">${random[i]}</span>`;
 }
 
-console.log(random);
+//Timer 10 secondi
+setTimeout(tempo10, 10000);
 
 
 
@@ -41,4 +41,14 @@ console.log(random);
 
 
 
+
+
+
+
+
+
+function tempo10()
+{
+    numeri.innerHTML = "";
+}
 // ----- FINE :) -----
